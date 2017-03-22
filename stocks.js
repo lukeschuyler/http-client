e8b6c1a863ab91d7d2352430abec4cd91c368ba7
Luke Schuyler <lukeschuyler1@gmail.com>
2017-03-22 13:43:41 -0500

    got parsed response data logged in terminal

--- stocks.js
+++ stocks.js
@@ -18,8 +18,15 @@ get(`http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=${args[0]}`,
     }
     if (error) {
     	console.log(error.message);
-    	// consume response data to free up memory, since we won't use the request body. Until the data is consumed, the 'end' event will not fire. Also, until the data is read it will consume memory that can eventually lead to a 'process out of memory' error.
     	res.resume();
     return;
     }
+    let body = '';
+	  res.on('data', (buff) => {
+	    console.log("status?", statusCode);
+	    body += buff.toString()
+	  });
+	  res.on('end', () => {
+	    console.log(JSON.parse(body));
+	  });
 })
