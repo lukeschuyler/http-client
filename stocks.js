36f84bb7f018cc7af8059f03ae51dc1af1112b85
Luke Schuyler <lukeschuyler1@gmail.com>
2017-03-22 13:40:32 -0500

    changed url and now receiving 200 statusCode

--- stocks.js
+++ stocks.js
@@ -3,8 +3,23 @@ const { readFile } = require('fs');
 const [,,...args] = process.argv;
 
 
-get(`http://dev.markitondemand.com/Api/v2/Quote/json?symbol=${args[0]}`, res => {
+get(`http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=${args[0]}`, res => {
 	const statusCode = res.statusCode;
     const contentType = res.headers['content-type'];
-    console.log(statusCode, contentType);
+
+
+    let error;
+    if (statusCode !== 200) {
+    	error = new Error(`Request Failed.\n` +
+                      `Status Code: ${statusCode}`);
+    } else if (!/^text\/javascript/.test(contentType)) {
+   	    error = new Error(`Invalid content-type.\n` +
+                      `Expected text/javascript but received ${contentType}`);
+    }
+    if (error) {
+    	console.log(error.message);
+    	// consume response data to free up memory, since we won't use the request body. Until the data is consumed, the 'end' event will not fire. Also, until the data is read it will consume memory that can eventually lead to a 'process out of memory' error.
+    	res.resume();
+    return;
+    }
 })
