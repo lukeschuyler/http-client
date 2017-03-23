f6336bc471169c56655e901042f5b128b54ea50f
Luke Schuyler <lukeschuyler1@gmail.com>
2017-03-23 09:52:21 -0500

    abstrascted and promsified getJSON function

--- stocks.js
+++ stocks.js
@@ -1,38 +1,48 @@
 const { get, createServer } = require('http');
 const { readFile } = require('fs');
 const [,,...args] = process.argv;
-const url = `http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters={"Normalized":false,"NumberOfDays":365,"DataPeriod":"Day","Elements":[{"Symbol":"${args[0]}","Type":"price","Params":["c"]}]}`
+const stocks = `http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters={"Normalized":false,"NumberOfDays":365,"DataPeriod":"Day","Elements":[{"Symbol":"${args[0]}","Type":"price","Params":["c"]}]}`
 
+const getJSON = url => {
+    return new Promise((resolve, reject) => {
+        get(url, res => {
+            resolve(res)
+        })
+    })
+}
 
-get(url, res => {
-	const statusCode = res.statusCode;
+const logPrice = res => {
+    const statusCode = res.statusCode;
     const contentType = res.headers['content-type'];
-    // console.log(res)
 
     let error;
     if (statusCode !== 200) {
-    	error = new Error(`Request Failed.\n` +
+        error = new Error(`Request Failed.\n` +
                       `Status Code: ${statusCode}`);
     } else if (!/^text\/javascript/.test(contentType)) {
-   	    error = new Error(`Invalid content-type.\n` +
+        error = new Error(`Invalid content-type.\n` +
                       `Expected text/javascript but received ${contentType}`);
     }
     if (error) {
-    	console.log(error.message);
-    	res.resume();
+        console.log(error.message);
+        res.resume();
     return;
     }
     let body = '';
-	  res.on('data', (buff) => {
-	    body += buff.toString()
-	  });
-	  res.on('end', () => {
-	    const data = JSON.parse(body);
-	    let quoteArray = data.Elements[0].DataSeries.close.values
-	    let sum = quoteArray.reduce((a, b) => {
-	    	return a + b
-	    })
-	    console.log((sum / quoteArray.length).toFixed(2))
-	  });
+      res.on('data', (buff) => {
+        body += buff.toString()
+      });
+      res.on('end', () => {
+        const data = JSON.parse(body);
+        let quoteArray = data.Elements[0].DataSeries.close.values
+        let sum = quoteArray.reduce((a, b) => {
+            return a + b
+        })
+        console.log((sum / quoteArray.length).toFixed(2))
+      });
+}
 
-})
+getJSON(stocks)
+  .then(data => {
+    logPrice(data)
+  })
